99eeab14380f703a82f6f20c68a7e24eb567a53b
genwox <98274424+genwox@users.noreply.github.com>
2026-05-25 16:59:51 -0400

    models.py finis

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from peewee import SqliteDatabase, Model, IntegerField, CharField, BooleanField, FloatField
+from peewee import SqliteDatabase, Model, IntegerField, CharField, BooleanField
+
 db = SqliteDatabase('database.db')
 
 class BaseModel(Model):
@@ -15,4 +16,37 @@ class Product(BaseModel):
     weight = IntegerField()
     price = IntegerField()
     in_stock = BooleanField()
+
+
+class Order(BaseModel):
+    id=IntegerField(primary_key=True)
+    quantity = IntegerField()
+    total_price = IntegerField(default=0)
+    total_price_tax = IntegerField(default=0)
+    shipping_price = IntegerField(default=0)
+    email = CharField(null=True)
+    paid = BooleanField(default=False)
+    product = IntegerField()
+
+    #Shipping
+    shipping_country = CharField(null=True)
+    shipping_province = CharField(null=True)
+    shipping_address = CharField(null=True)
+    shipping_city = CharField(null=True)
+    shipping_postal_code = CharField(null=True)
+
+    #Credit Card
+    cc_name = CharField(null=True)
+    cc_first_digits = CharField(null=True)
+    cc_last_digits = CharField(null=True)
+    cc_expiration_year = CharField(null=True)
+    cc_expiration_month = CharField(null=True)
+
+    #Transaction
+    transaction_id = CharField(null=True)
+    transaction_success = BooleanField(null=True)
+    transaction_amount_charged = IntegerField(null=True)
+
+
+
 
